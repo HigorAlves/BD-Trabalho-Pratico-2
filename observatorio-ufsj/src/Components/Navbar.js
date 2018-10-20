c3d14204333ada6eaf314105d2c7ce39912d7e5e
Higor Alves <goodyearalves@gmail.com>
2018-10-20 08:42:15 -0300

    update

--- observatorio-ufsj/src/Components/Navbar.js
+++ observatorio-ufsj/src/Components/Navbar.js
@@ -24,7 +24,7 @@ export default class NavBar extends Component {
 					<ul className="navbar-nav mr-auto">
 						<li className="nav-item active">
 							<a className="nav-link" href="#">
-								Home <span className="sr-only">(current)</span>
+								Inicio <span className="sr-only">(current)</span>
 							</a>
 						</li>
 						<li className="nav-item">
