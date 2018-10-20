6e9315d3a369812f751512a903af01b9b6be4e90
Higor Alves <goodyearalves@gmail.com>
2018-10-20 08:30:12 -0300

    Home pronta

--- observatorio-ufsj/src/Routes/Router.js
+++ observatorio-ufsj/src/Routes/Router.js
@@ -5,16 +5,17 @@ import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import PrivateRoute from '../Components/PrivateRoute';
 
 import Home from '../Views/Home';
+import ObservatorioHome from '../Views/Observatorio';
 
 export default class Routers extends Component {
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <Route exact path='/' component={Home} />
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/observatorio" component={ObservatorioHome} />
+				</div>
+			</Router>
+		);
+	}
 }
---
+++ observatorio-ufsj/src/Views/Observatorio.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class ObservatorioHome extends Component {
+	render() {
+		return (
+			<div>
+				<p>oal</p>
+			</div>
+		);
+	}
+}
