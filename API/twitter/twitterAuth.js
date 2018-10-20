ba500a10814c8fce23038c30bdba819e99b1e927
Higor Alves <HigorAlves@users.noreply.github.com>
2018-10-20 01:02:49 -0300

    delete

--- API/twitter/twitterAuth.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: "ta48mVMyQ3R4ai98VHBhhMJDg",
-  consumer_secret: "3Xj69GvoLNqRVhRuXFVCZNJ0pVceJ7eTFYusSYlcatjtxQMEK2",
-  access_token: "535826909-nxUNEcKHmY1Xcv18npYzMLkrgZOrSuriJfQpcHCJ",
-  access_token_secret: "PYgnmt0Cfy65Bgu3kk4u5dZvbNjvweCeKwkWnRbT2onm0"
-}
