59492b8b59e096657edd83fae2307b0c68e1cb60
Higor Alves <Goodyearalves@gmail.com>
2018-10-19 22:50:54 -0300

    Mongo TweetScheme

--- API/twitter/index.js
+++ API/twitter/index.js
@@ -3,7 +3,7 @@ const twitterAuth = require('./twitterAuth');
 const { bolsonaroTweets, haddadTweets } = require('./configs/params');
 const T = new Twit(twitterAuth);
 
-let tweetID = undefined;
+let tweetID = null;
 let tweetsToGet = bolsonaroTweets;
 
 errorMenssage = (mensagem) => {
@@ -32,70 +32,36 @@ gotData = (err, data, response) => {
     // console.log(data.user.profile_image_url_https);
     // console.log(data.user.profile_banner_url);
   });
+  return;
 }
 
-if (tweetID === undefined) {
+if (tweetID === null) {
   tweetsToGet = bolsonaroTweets;
 } else {
   tweetsToGet = Object.assign({ max_id: tweetID }, bolsonaroTweets)
 }
 
-// var getTweets = new Promise(
-//   function (resolve, reject) {
-//     // console.log('Promisse iniciada!');
-//     resolve(
-//       tweetId = T.get('statuses/user_timeline', tweetsToGet, gotData)
-//     );
-//     reject();
-//   }
-// )
-
-// getTweets.then(function (val) {
-//   console.log('ola mundo' + val)
-// })
-
-// var promise1 = new Promise(function (resolve, reject) {
-//   resolve(
-//     T.get('statuses/user_timeline', tweetsToGet, gotData);
-//   );
-// });
-
-
-// promise1.then(function (value) {
-//   console.log(tweetID);
-// });
-
-
-var promise1 = () => new Promise(function (resolve, reject) {
-  T.get('statuses/user_timeline', tweetsToGet, (err, data, response) => {
+var getTweets = new Promise(function (resolve, reject) {
+  T.get('statuses/user_timeline', tweetsToGet, function (err, data, response) {
     if (err) {
       throw errorMenssage('Aconteceu algum erro na hora de contatar a API \n' + err);
     }
-
     data.map((data) => {
       tweetID = data.id;
-      // console.log('tweetID: ' + tweetID);
-      // console.log(data.id);
-      // console.log(data.full_text);
-      // console.log(data.entities);
-      // console.log(data.coordinates);
-      // console.log(data.retweet_count);
-      // console.log(data.favorite_count);
-      // console.log(data.user.name);
-      // console.log(data.user.screen_name);
-      // console.log(data.user.location);
-      // console.log(data.user.followers_count);
-      // console.log(data.user.verified);
-      // console.log(data.user.profile_image_url_https);
-      // console.log(data.user.profile_banner_url);
     });
+    resolve(data)
   })
-  resolve(data)
+
 });
 
 
-promise1().then(function (value) {
-  console.log(tweetID);
-});
+async function pegarTweets() {
+  await getTweets.then(function (value) {
+    console.log('Promisse 1');
+  });
+}
 
-  // T.get('statuses/user_timeline', tweetsToGet, gotData);
+
+pegarTweets();
+
+//T.get('statuses/user_timeline', tweetsToGet, gotData)
